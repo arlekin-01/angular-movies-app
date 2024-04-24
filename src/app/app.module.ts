5dbed89d1ac6fcc2fad6354bc3aad48d4ee7f8f5
HpFreake-01 <maxchuprun01@gmail.com>
2024-04-24 15:16:13 +0200

    add bootstrap styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DetailsComponent } from './components/details/details.component';
+import { DetailsMoviesComponent } from './components/details-movies/details-movies.component';
+import { DetailsReviewsComponent } from './components/details-reviews/details-reviews.component';
+import { NotAuthorizedComponent } from './components/not-authorized/not-authorized.component';
+import { SearchComponent } from './components/search/search.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DetailsComponent,
+    DetailsMoviesComponent,
+    DetailsReviewsComponent,
+    NotAuthorizedComponent,
+    SearchComponent,
+    LoaderComponent,
+    ModalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
